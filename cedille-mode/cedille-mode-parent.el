0b825da953b55eaebe7ca96485de4bea5d392b85
guneratne <guneratne@bd178f5f-7bea-4014-844f-495ca19b8805>
2016-08-26 17:59:04 +0000

    CED-63: Implemented feature. Now when buffers are resized, they will stay locked at the new size. They can be unlocked (and returned to the minimum necessary size) by pressing '='
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/lambda-encodings/implementations/cedille@23427 bd178f5f-7bea-4014-844f-495ca19b8805

--- cedille-mode/cedille-mode-parent.el
+++ cedille-mode/cedille-mode-parent.el
@@ -1,13 +1,24 @@
 ;;; Code for the parent keymap from which keymaps of other minor modes will be derived.
 
 (defmacro make-cedille-mode-resize-current-window (amount)
-  "Creates a function that resizes the window associated with buffer by amount"
-  `(lambda() (interactive) (window-resize nil ,amount)))
+  "Creates a function that resizes the window associated with buffer by amount and then locks the size of the window"
+  `(lambda()
+     (interactive)
+     (with-current-buffer (current-buffer) (setq window-size-fixed nil))
+     (window-resize nil ,amount)
+     (with-current-buffer (current-buffer) (setq window-size-fixed t))))
+
+(defun cedille-mode-unlock-current-window-size()
+  "Allows selected window to be resized, then restores it to the minimal size"
+  (interactive)
+  (with-current-buffer (current-buffer) (setq window-size-fixed nil))
+  (fit-window-to-buffer))
 
 (defvar cedille-mode-minor-mode-parent-keymap
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "+") (make-cedille-mode-resize-current-window 1))  ; increase size of window
-    (define-key map (kbd "-") (make-cedille-mode-resize-current-window -1)) ; decrease size of window
+    (define-key map (kbd "+") (make-cedille-mode-resize-current-window 1))  ; increase and lock size of window
+    (define-key map (kbd "-") (make-cedille-mode-resize-current-window -1)) ; decrease and lock size of window
+    (define-key map (kbd "=") #'cedille-mode-unlock-current-window-size)    ; unlock size of window then resizes it
     map))
 
 (provide 'cedille-mode-parent)
