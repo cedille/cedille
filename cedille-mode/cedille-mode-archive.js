5151c5e498200c346bc932c0e66407c805161dff
Carl Olson <CarlOlson@users.noreply.github.com>
2019-01-02 18:48:24 -0600

    archive show span data on click

--- cedille-mode/cedille-mode-archive.js
+++ cedille-mode/cedille-mode-archive.js
@@ -1,15 +1,45 @@
 
 const cedilleSpans = JSON.parse(document.getElementById('spans').innerHTML);
+const cedilleData = document.getElementById('cedille-data');
 const htmlSpans = [...document.getElementsByClassName('cedille-span')];
 
 function removeClassFromSpans(className) {
   htmlSpans.forEach(htmlSpan => htmlSpan.classList.remove(className));
 }
 
-htmlSpans.forEach(htmlSpan => {
+function displayData({name, start, end, data}) {
+  cedilleData.innerHTML = null;
+
+  const displayData = {
+    name,
+    start,
+    end,
+    ...data
+  };
+
+  Object.keys(displayData).forEach((key) => {
+    var bodyDiv = document.createElement('div');
+    var keyDiv = document.createElement('div');
+    var valueDiv = document.createElement('div');
+
+    keyDiv.innerText = key;
+    keyDiv.className = 'cedille-key';
+
+    valueDiv.innerText = displayData[key];
+    valueDiv.className = 'cedille-value';
+
+    bodyDiv.className = 'cedille-pair';
+    bodyDiv.appendChild(keyDiv);
+    bodyDiv.appendChild(valueDiv);
+    cedilleData.appendChild(bodyDiv);
+  });
+}
+
+htmlSpans.forEach((htmlSpan, index) => {
   htmlSpan.addEventListener('click', () => {
     removeClassFromSpans('highlight');
     htmlSpan.classList.add('highlight');
+    displayData(cedilleSpans[index]);
   }, { capture: true });
 
   htmlSpan.addEventListener('mouseover', () => {
