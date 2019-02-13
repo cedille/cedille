248104c4c0eed7f2aeae575143bef4ae1990a25a
Carl Olson <CarlOlson@users.noreply.github.com>
2019-02-13 15:43:42 -0600

    refactor archive to remove lexical-let

--- cedille-mode/cedille-mode-archive.el
+++ cedille-mode/cedille-mode-archive.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 ;;; cedille-mode-archive.el --- Archive a program to JSON and HTML
 
 (require 'json)
@@ -14,30 +15,30 @@
 (defconst cedille-mode-archive-javascript
   (cedille-mode-archive-read-file "cedille-mode-archive.js"))
 
-(lexical-let (buffer-name buffer-file-name)
-  (defun cedille-mode-archive ()
-    (interactive)
-    (unless (eq major-mode 'cedille-mode)
-      (user-error "Must be using cedille mode!"))
-    (setq buffer-name (buffer-name)
-          buffer-file-name (buffer-file-name))
-    (run-with-timer 1 nil 'cedille-mode-archive-timer))
+(defun cedille-mode-archive ()
+  (interactive)
+  (unless (eq major-mode 'cedille-mode)
+    (user-error "Must be using cedille mode!"))
+  (run-with-timer 1 nil
+                  'cedille-mode-archive-timer
+                  (buffer-name)
+                  (buffer-file-name)))
 
-  (defun cedille-mode-archive-timer ()
-    (with-current-buffer buffer-name
-      (when cedille-mode-caching
-        (user-error "Must wait for caching to finish!"))
-      (se-inf-interactive
-       (cedille-mode-concat-sep "archive" buffer-file-name)
-       (lambda (archive extra)
-         (let ((archive-file-name (concat buffer-name ".html")))
-           (with-temp-file archive-file-name
-             (insert (format cedille-mode-archive-html-template
-                             archive
-                             cedille-mode-archive-javascript)))
-           (message "Saved archive as %s" archive-file-name)))
-       nil
-       :header "Archiving"))))
+(defun cedille-mode-archive-timer (buffer-name buffer-file-name)
+  (with-current-buffer buffer-name
+    (when cedille-mode-caching
+      (user-error "Must wait for caching to finish!"))
+    (se-inf-interactive
+     (cedille-mode-concat-sep "archive" buffer-file-name)
+     (lambda (archive extra)
+       (let ((archive-file-name (concat buffer-name ".html")))
+         (with-temp-file archive-file-name
+           (insert (format cedille-mode-archive-html-template
+                           archive
+                           cedille-mode-archive-javascript)))
+         (message "Saved archive as %s" archive-file-name)))
+     nil
+     :header "Archiving")))
 
 (provide 'cedille-mode-archive)
 
