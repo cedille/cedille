f6251b2569bc4559f97fce87cf15575f48e30c51
Pedro Abreu <abreu223@hotmail.com>
2019-09-02 14:25:32 -0400

    Synthesize forall and pi

--- cedille-mode/cedille-mode-synthesis.el
+++ cedille-mode/cedille-mode-synthesis.el
@@ -9,6 +9,11 @@
 (defun get-span-name(data)
   (cdr (assoc 'name data)))
 
+(defun synth-foralls(type)
+  (replace-regexp-in-string "∀" "Λ" type)
+  (replace-regexp-in-string "Π" "λ" type)
+  )
+
 (defun cedille-mode-synth-quantifiers ()
   "This function will synthesize the proper lambdas that match
 the quantifiers at the given hole"
@@ -18,9 +23,10 @@ the quantifiers at the given hole"
            (d (se-term-to-json term))
            (name (se-term-name term))
            (type (get-span-type d))
+           (synth-type (synth-foralls type))
            )
       (when (string= name 'Hole)
-        (insert-before-markers type))
+        (insert-before-markers synth-type))
       )))
 
 (provide 'cedille-mode-synthesis)
